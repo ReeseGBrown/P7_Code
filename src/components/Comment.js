70f9af6d83a5bafb197dc9c0812aa39fced7a7cc
Reese Brown <reesegbrown@gmail.com>
2024-11-09 01:14:51 -0600

    Started work on like/dislike functionality

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,24 @@
 function Comment ({ comment_id, comment_content }) {
+    
+    const submitFunction = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:4000/api/comment/' + comment_id , {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            }
+        }).then((res) => res.json())
+      .catch((err) => console.log('error submitting'))
+        
+    } 
+    
     return (
         <li className = 'comment_card'>
             <h4>userName</h4>
             <p>{comment_content}</p>
             {/* this is gonna be an unordered list w/ key props*/} 
             <div className = 'comment_card_bottom'>
-                <button type="submit">Delete</button>
+                <button type="submit" onClick={submitFunction}>Delete</button>
             </div>
         
         </li>
--- src/components/homepage_post.js
+++ src/components/homepage_post.js
@@ -1,7 +1,43 @@
 import '../styles/homepage_post.css'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 function Post ({ post_id, post_title, post_content}) {
     
+    const [likeData, setLikeData] = useState({});
+
+    useEffect(() => {
+        async function testGetter() {
+            try {
+                const response = await fetch("http://localhost:4000/api/likes/" + post_id)
+                const data = await response.json()
+                //console.log(data);
+                setLikeData(data);
+            }
+            catch(err) {
+                console.log("err");
+            }
+        }
+        testGetter()
+        //console.log("leng " + testData.length);
+     }, [])
+
+    const submitLike = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:4000/api/likes/' , {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user_id: 5,
+                post_id: post_id
+            }),
+        }).then((res) => res.json())
+      .catch((err) => console.log('error submitting'))
+        
+    } 
+
     return (
         <li className = 'post_card'>
             <h4>userName</h4>
@@ -9,7 +45,8 @@ function Post ({ post_id, post_title, post_content}) {
             <p>{post_content}</p>
             {/* this is gonna be an unordered list w/ key props*/} 
             <div className = 'post_card_bottom'>
-                <p>likes</p>
+                <button type="submit" onClick={submitLike}>Like</button>
+                <p>{likeData.length}</p>
             </div>
         
         </li>        
